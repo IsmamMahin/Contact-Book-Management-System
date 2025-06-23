b364bea11bc625803628abe83f640998206e1780
IsmamAhnaf <ismam.mahin@northsouth.edu>
2025-06-23 22:10:41 +0600

    Menu added

---
+++ main.py
@@ -0,0 +1,4 @@
+from menu import menu
+#print("Menu is loaded")
+menu()
+
---
+++ menu.py
@@ -0,0 +1,10 @@
+def menu():
+    print("Welcome to the Contact Book CLI System!")
+    print("Loading contacts from contacts.csv... Done!")
+    print("=========== MENU ===========")
+    print("1. Add Contact")
+    print("2. View Contacts")
+    print("3. Search Contact")
+    print("4. Remove Contact")
+    print("5. Exit")
+    print("============================")
