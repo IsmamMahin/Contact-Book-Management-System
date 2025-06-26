a2b86f05d36e9ae90ba885b429feb93fc84c7269
IsmamMahin <ismam.mahin@northsouth.edu>
2025-06-26 21:34:31 +0600

    Add clear scream when the use of a function is complete, for better presentation

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
+import os
 import csv
-from menu import menu
+from menu import menu, wait_for_enter, clear_console
 from add_contact import add_contact
 from search_contact import search_contact
 from view_contact import view_contact
 from remove_contact import remove_contact
 from save_to_file import save_contacts_to_file, load_contacts_from_file
 
+clear_console()
 contacts = load_contacts_from_file()
 while True:
 #Menu is loaded
@@ -22,20 +24,34 @@ while True:
     if choice == 1:
         add_contact(contacts)
         save_contacts_to_file(contacts)
+        wait_for_enter()
+        clear_console()
+
 
     elif choice == 2:
         view_contact(contacts)
+        wait_for_enter()
+        clear_console()
+
 
     elif choice == 3:
         search_term = input("Enter search term (name/email/phone):")
         search_contact(contacts, search_term)
+        wait_for_enter()
+        clear_console()
+
 
     elif choice == 4:
         contacts = load_contacts_from_file()
         remove_contact(contacts)
+        wait_for_enter()
+        clear_console()
+
 
     elif choice == 5:
         print("Exiting Contact Book. Goodbye!")
         break
     else:
         print("Invalid choice. Please try again.")
+        wait_for_enter()
+        clear_console()
--- menu.py
+++ menu.py
@@ -1,3 +1,4 @@
+import os
 #Menu of the system
 def menu():
     print("Welcome to the Contact Book CLI System!")
@@ -9,3 +10,11 @@ def menu():
     print("4. Remove Contact")
     print("5. Exit")
     print("============================")
+
+def wait_for_enter(prompt="Press Enter to continue..."):
+    #Pauses the program execution until the user presses Enter.
+    input(prompt)
+
+#clears the screen
+def clear_console():
+    os.system('cls' if os.name == 'nt' else 'clear')
